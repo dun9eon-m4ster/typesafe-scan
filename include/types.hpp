abf2b55617b7ddecacd8038b837dea5a58cc712d
Егор <egor1999x@mail.ru>
2026-05-11 17:17:09 +0300

    Замена созданной системы типов на одну функцию
    Концепты для числовых и строковых типов
    
    Переиспользование трейта для проверки строки в трейте поддерживаемых типов

--- include/types.hpp
+++ include/types.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <concepts>
 #include <cstdint>
 #include <limits>
 #include <string>
@@ -10,69 +11,6 @@
 
 namespace stdx::details {
 
-enum class conversion_specifier { unknown, empty, d, s, u, f };
-
-constexpr conversion_specifier cs_from_string(std::string_view string) {
-    if (string == "")
-        return conversion_specifier::empty;
-    if (string == "d")
-        return conversion_specifier::d;
-    if (string == "s")
-        return conversion_specifier::s;
-    if (string == "u")
-        return conversion_specifier::u;
-    if (string == "f")
-        return conversion_specifier::f;
-
-    return conversion_specifier::unknown;
-}
-
-template <typename T, conversion_specifier spec>
-struct is_compatable {
-    static constexpr bool value = false;
-};
-
-template <typename T, conversion_specifier spec>
-static constexpr bool is_compatable_v = is_compatable<T, spec>::value;
-
-template <typename T>
-struct is_compatable<T, conversion_specifier::d> {
-    static constexpr bool value = std::numeric_limits<T>::is_integer;
-};
-
-template <typename T>
-struct is_compatable<T, conversion_specifier::u> {
-    static constexpr bool value = std::numeric_limits<T>::is_integer && !std::is_signed_v<T>;
-};
-
-template <typename T>
-struct is_compatable<T, conversion_specifier::f> {
-    static constexpr bool value = std::is_floating_point_v<T>;
-};
-
-template <typename T>
-struct is_compatable<T, conversion_specifier::s> {
-    static constexpr bool value = std::is_same_v<T, std::string> || std::is_same_v<T, std::string_view>;
-};
-
-template <typename T>
-struct is_compatable<T, conversion_specifier::empty> {
-    static constexpr bool value = true;
-};
-
-template <typename... Ts>
-struct is_supported {
-    static constexpr bool value =
-        ((std::is_same_v<Ts, int8_t> || std::is_same_v<Ts, int16_t> || std::is_same_v<Ts, int32_t> ||
-          std::is_same_v<Ts, int64_t> || std::is_same_v<Ts, uint8_t> || std::is_same_v<Ts, uint16_t> ||
-          std::is_same_v<Ts, uint32_t> || std::is_same_v<Ts, uint64_t> || std::is_same_v<Ts, float> ||
-          std::is_same_v<Ts, double> || std::is_same_v<Ts, std::string> || std::is_same_v<Ts, std::string_view>) &&
-         ...);
-};
-
-template <typename... Ts>
-static constexpr bool is_supported_v = is_supported<Ts...>::value;
-
 template <typename T>
 struct is_string {
     static constexpr bool value = std::is_same_v<T, std::string> || std::is_same_v<T, std::string_view>;
@@ -80,6 +18,45 @@ struct is_string {
 
 template <typename T>
 static constexpr bool is_string_v = is_string<T>::value;
+
+template <typename... Ts>
+struct is_supported {
+    static constexpr bool value =
+        ((std::is_same_v<Ts, int8_t> || std::is_same_v<Ts, int16_t> || std::is_same_v<Ts, int32_t> ||
+          std::is_same_v<Ts, int64_t> || std::is_same_v<Ts, uint8_t> || std::is_same_v<Ts, uint16_t> ||
+          std::is_same_v<Ts, uint32_t> || std::is_same_v<Ts, uint64_t> || std::is_same_v<Ts, float> ||
+          std::is_same_v<Ts, double> || is_string_v<Ts>) &&
+         ...);
+};
+
+template <typename... Ts>
+static constexpr bool is_supported_v = is_supported<Ts...>::value;
+
+template <typename T>
+constexpr bool is_compatible(std::string_view spec) {
+    if (spec == "d")
+        return std::numeric_limits<T>::is_integer;
+
+    if (spec == "u")
+        return std::numeric_limits<T>::is_integer && !std::is_signed_v<T>;
+
+    if (spec == "f")
+        return std::is_floating_point_v<T>;
+
+    if (spec == "s")
+        return is_string_v<T>;
+
+    if (spec == "")
+        return true;
+
+    return false;
+}
+
+template <typename T>
+concept numeric = std::integral<T> || std::floating_point<T>;
+
+template <typename T>
+concept stringlike = is_string_v<T>;
 
 // Класс для хранения ошибки неуспешного сканирования
 
